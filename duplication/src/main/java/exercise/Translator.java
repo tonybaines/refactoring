4ee1e898d4110355869599c77a4da5e29c926791
Tony Baines <tony.baines.1@gmail.com>
2011-06-20 22:12:53 +0100

    Added a presentation

--- duplication/src/main/java/exercise/Translator.java
+++ duplication/src/main/java/exercise/Translator.java
@@ -1,5 +1,8 @@
 package exercise;
 
+/*
+ * This class has been 'defactored' deliberately as a starting point for practice
+ */
 public class Translator {
     private static final String[] DIGITS = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
     private static final String[] TENS = {"NaN", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"};
@@ -18,27 +21,27 @@ public class Translator {
 		} else if (i < 1000) {
 			if (i%100 > 0)
 	        	if (i%100 < 100)
-	                return translate(i/100) + " " + "hundred" +" and"  + " " + translate(i%100);
+	                return translate(i/100) + " hundred and " + translate(i%100);
 	            else
-	                return translate(i/100) + " " + "hundred" +" " + translate(i%100);
+	                return translate(i/100) + " hundred " + translate(i%100);
 	        else
-	            return translate(i/100) + " " + "hundred";
+	            return translate(i/100) + " hundred";
 		} else if (i < 1000000) {
 	        if (i%1000 > 0)
 	        	if (i%1000 < 100)
-	                return translate(i/1000) + " " + "thousand" +" and"  + " " + translate(i%1000);
+	                return translate(i/1000) + " thousand and " + translate(i%1000);
 	            else
-	                return translate(i/1000) + " " + "thousand" +" " + translate(i%1000);
+	                return translate(i/1000) + " thousand " + translate(i%1000);
 	        else
-	            return translate(i/1000) + " " + "thousand";
+	            return translate(i/1000) + " thousand";
 		} else if (i < 1000000000) {
 			if (i%1000000 > 0)
 	        	if (i%1000000 < 100)
-	                return translate(i/1000000) + " " + "million" + " and"  + " " + translate(i%1000000);
+	                return translate(i/1000000) + " million and " + translate(i%1000000);
 	            else
-	                return translate(i/1000000) + " " + "million" + " " + translate(i%1000000);
+	                return translate(i/1000000) + " million " + translate(i%1000000);
 	        else
-	            return translate(i/1000000) + " " + "million";
+	            return translate(i/1000000) + " million";
 		} else
             throw new IllegalArgumentException("I don't understand numbers bigger than " + 1000000000);
     }
